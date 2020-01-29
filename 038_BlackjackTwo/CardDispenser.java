84fad6c97fb6baad635e122301a0448e22f4e38a
Kara Conrad <kc1610047@036Labs-iMac.local>
2020-01-29 10:01:17 -0600

    hey

--- 038_BlackjackTwo/CardDispenser.java
+++ 038_BlackjackTwo/CardDispenser.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
-
+// import because I will make a scanner object later
 public class CardDispenser{
 
 int total;
+int dealerCard;
+// in this class I set 2 variables to be used throughout the class anywhere
 
     public int getCard(){
-    int firstTwo = Math.random();
+    // return type is int for my card method I defined
+    double firstTwo = Math.random();
 
         if (firstTwo <=.1){
             if (firstTwo <= .5){
@@ -41,39 +44,57 @@ int total;
             return 10;
         }
 
+        // if else statement along with random math method to random return a card value
+
     }
 
-    public hit(int total){
+    public String hit(int total){
+        // a second method that basically does the rest of the game work, return type string
         this.total = total;
         Scanner myScan = new Scanner(System.in);
-        boolean hit;
-
+        boolean hit=true;
+        // I need a boolean variable, a scanner and the parameter (an int) to be defined as the previously made total variable
         while (hit == true){
 
-        System.out.println("Would you like to hit or keep your total of " + total + ";");
+       System.out.println("Would you like to hit or keep your total of " + total + "?");
         String response = myScan.nextLine();
+        // I use scanner to set string response to the users input for what action they want
 
-        if (response.compareTo("hit")){
+        if (response.compareTo("hit")==0){
+        // in the while loop I use an if else statement, if they hit it goes into a second if statement after calling the getCard method to add to total
             total += getCard();
-                if (total>=21){
+                if (total>21){
+                    hit = false;
+                    System.out.println("Your number is over 21. It is " + total + ".");
                 }
-                else{}
-            hit = true; 
         }
-        else if (response.compareTo("keep")){
+        else if (response.compareTo("keep")==0){
             hit = false;
         }
         else {
-            System.out.println("You've entered an invalid command. Try again please. ");
-            
+            System.out.println("You've entered an invalid command. Try again please.");
+        
+            myScan.close();
+        // I manipulate hit to false when the person is either over 21 or press keep, this breaks the loop
+    
+        }}
+
+        
+        System.out.println("The dealers value is " + dealerCard + ".");
+        // finally reveal the dealers value in print statement
+
+        if (total > dealerCard){
+            return "Congrats, you won!";}
+        else if (total < dealerCard){
+            return "Sorry, you lost.";}
+        else if (dealerCard>21){
+            return "You win! The dealers told was over 21 being " + dealerCard;}
+        else {
+            return "A tie! You both have " + total;}
+        // after the loop there is yet another if else statement comparing the values of user and dealers cards and returning a string to say who won
         }
 
-
-
-
-
-            }
-        }
-    }
-
+        public static void main(String[]args){}
+        // PSVMSA is needed for every class, it doesn't do anything in this instance
+}
 
--- 038_BlackjackTwo/Table.java
+++ 038_BlackjackTwo/Table.java
@@ -1,14 +1,25 @@
+import java.util.Scanner;
+// need for scanner in hit object to carry over
+
 public class Table{
     public static void main(String[]args){
 
         CardDispenser cd = new CardDispenser();
+        // create an instance of the class
+
         int cardOne = cd.getCard();
         int cardTwo = cd.getCard();
-
+        // get two cards for user to start with using get card method from card dispenser class
         cd.total = cardOne + cardTwo;
-        System.out.print("You have a " + cardOne + " and a " + cardTwo + ". Your total value right now is: " + total);
 
-        int dealerCard = cd.getCard();
-        System.out.println("The dealers card and value is " + dealerCard);
+        System.out.println("You have a " + cardOne + " and a " + cardTwo + ". Your total value right now is: " + cd.total);
+        // let them know what their current total is 
+        while (cd.dealerCard <= 16){
+            cd.dealerCard = cd.dealerCard + cd.getCard();
+        // while statement to set the dealers value, condiition for it to loop is number must be lower than 16
+        }
+
+        System.out.println(cd.hit(cd.total));
+        // print the hit method from card dispenser using total varaible accessible everywhere, which is returning a string
     }
 }
